8e1e82c38474986355b4344fec034421f7c24a5d
rash0x6964 <rash0x6964@gmail.com>
2024-02-20 19:27:32 +0100

    build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,8 +5,8 @@ function ColorModeSwitch() {
 
   return (
     <HStack>
-      <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
-      <Text>Dark Mode</Text>
+      <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "../components/ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 function NavBar() {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <Text>Game Hub</Text>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
