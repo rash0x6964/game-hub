eb72c2fc3b118f0094200da711d31f00a0f586d9
rash0x6964 <rash0x6964@gmail.com>
2024-02-20 16:30:57 +0100

    display genres loading...

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 
 function GameGrid() {
   const { data, error, isLoading } = useGame();
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
   return (
     <>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,30 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Text,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
 import useGenre from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
-  const { data } = useGenre();
+  const { data, isLoading } = useGenre();
+  const skeletons = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14];
 
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem key={skeleton} paddingY="5px">
+            <HStack>
+              <Skeleton boxSize="40px" borderRadius={8} />
+              <SkeletonText width="100%" noOfLines={2} borderRadius={8}/>
+            </HStack>
+          </ListItem>
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
