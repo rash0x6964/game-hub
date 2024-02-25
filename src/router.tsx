72d2bc6b6c5b91f0a90a06ff6b19b6da07a25f14
rash0x6964 <rash0x6964@gmail.com>
2024-02-25 18:55:55 +0100

    handle errors

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
     ],
+    errorElement: <ErrorPage />,
   },
 ]);
 
