8b252d339709c7b4a6127693256cc64aede8b0c7
rash0x6964 <rash0x6964@gmail.com>
2024-02-26 21:35:07 +0100

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 function GameDetailPage() {
   const { slug } = useParams();
@@ -15,7 +16,8 @@ function GameDetailPage() {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-	  <GameAttributes game={game} />
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 }
